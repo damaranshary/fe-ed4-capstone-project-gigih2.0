53cec0eebd9e6ef9049d05dc8e76624dcf88331b
damaranshary <damar.galihanshary7@gmail.com>
2022-06-24 21:06:02 +0700

    updating routing route

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,8 +2,12 @@ import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import Navbar from "../component/navbar";
 import Home from "../pages/home";
 import Quiz from "../pages/quiz";
-import Video from "../pages/video";
-import Books from "../pages/books";
+import Watch from "../pages/watch";
+import Read from "../pages/read";
+import Learn from "../pages/learn";
+import Legends from "../component/legends";
+import FunFact from "../component/funFact"
+import BookDescription from "../component/books/description";
 
 
 const AppRouter = () => {
@@ -11,10 +15,25 @@ const AppRouter = () => {
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/books" element={<Books />} />
-                <Route path="/quiz" element={<Quiz />} />
-                <Route path="/video" element={<Video />} />
+                <Route path="/">
+                    <Route index element={<Home />} />
+                    <Route path="read">
+                        <Route index element={<Read />} />
+                        <Route path="legends">
+                            <Route index element={<Legends />} />
+                            <Route path=":id">
+                                <Route index element={<BookDescription />} />
+                                <Route path=":pages" element={<Legends />}/>
+                            </Route>
+
+                        </Route>
+
+                        <Route path="fun-fact" element={<FunFact />} />
+                    </Route>
+                    <Route path="quiz" element={<Quiz />} />
+                    <Route path="watch" element={<Watch />} />
+                    <Route path="learn" element={<Learn />} />
+                </Route>
             </Routes>
         </Router>
     )
