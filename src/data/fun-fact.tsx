16138215bbcdb55a9d9ff15d3eae5fd08e1c6878
damaranshary <damar.galihanshary7@gmail.com>
2022-06-24 21:05:03 +0700

    add data dummy for books

---
+++ src/data/fun-fact.tsx
@@ -0,0 +1,19 @@
+const funFactData = [
+    {
+        isNew: true,
+        imageURL:
+            'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Fun Fact Danau Toba',
+        id: "FF0127062022",
+        category: "fun-fact",
+    },
+    {
+        isNew: false,
+        imageURL: 'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Fun Fact Sukses',
+        id: "FF0126062022",
+        category: "fun-fact"
+    }
+];
+
+export default funFactData;
---
+++ src/data/legends.tsx
@@ -0,0 +1,34 @@
+const legendsData = [
+    {
+        isNew: true,
+        imageURL:
+            'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Legenda Danau Toba',
+        id: "L0227062022",
+        category: "legends"
+    },
+    {
+        isNew: false,
+        imageURL: 'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Legenda Sukses',
+        id: "L0126062022",
+        category: "legends"
+    },
+    {
+        isNew: true,
+        imageURL:
+            'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Legenda Danau Toba',
+        id: "L0227062022",
+        category: "legends"
+    },
+    {
+        isNew: false,
+        imageURL: 'https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80',
+        name: 'Legenda Sukses',
+        id: "L0126062022",
+        category: "legends"
+    }
+];
+
+export default legendsData;
