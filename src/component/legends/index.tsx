4bfa40c85161cd822fdd996dfc27bdd295d6c9dc
damaranshary <damar.galihanshary7@gmail.com>
2022-06-28 22:36:38 +0700

    add fetchdata using array (still failed)

--- src/component/legends/index.tsx
+++ src/component/legends/index.tsx
@@ -2,22 +2,42 @@ import { Container, SimpleGrid, Heading, Link, Box } from "@chakra-ui/react";
 import { Link as ReactRouterLink } from "react-router-dom";
 import Books from '../books';
 import legendsData from '../../data/legends'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BooksDataProps } from "../../types/types";
 
 const Legends = () => {
+    const legendsBook = ['/data/B001.json', '/data/B002.json', '/data/B003.json']
+    const [legendsData, setLegendsData] = useState<BooksDataProps[]>([]);
+
+    useEffect(() => {
+        legendsBook.map((item) => {
+            fetchLegendsData(item).then(res => setLegendsData([...legendsData, res]));
+            console.log(item);
+        })
+    }, [])
+
+    const fetchLegendsData = async (pathURL: string): Promise<BooksDataProps> => {
+        const data: any = await axios
+            .get(pathURL)
+            .catch(err => console.log(err));
+        return data.data;
+    }
+
     return (
         <Container maxW='6xl' centerContent>
             <Link as={ReactRouterLink} to='/read/legends'><Heading>Legends</Heading></Link>
             <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing="1">
-                {legendsData !== undefined && legendsData.map((item) => {
-                    return (
-                        <Books
-                            coverImageURL={item.coverImageURL}
-                            content={item.content}
-                            name={item.name}
-                            id={item.id}
-                            category={item.category} />
-                    )
-                })}
+                {legendsData !== undefined && legendsData.map((item) =>
+                    <Books
+                        coverImageURL={item.coverImageURL}
+                        name={item.name}
+                        id={item.id}
+                        description={item.description}
+                        category={item.category}
+                        content={item.content}
+                    />
+                )}
             </SimpleGrid>
 
         </Container>
