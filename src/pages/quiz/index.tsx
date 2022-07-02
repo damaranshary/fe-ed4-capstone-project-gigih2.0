ea8b78cf4fe4c1af6bc7c97b9ea6cecac91df7ae
Naufal Taufiqurrahman <naufaltq16@gmail.com>
2022-07-02 10:46:03 +0700

    tidy up the footer and make a style quiz

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,42 +1,43 @@
 import { Container } from "@chakra-ui/react";
 import { useState } from "react";
+import "./quiz.css";
 
 const Quiz = () => {
   const questions = [
     {
-      questionText: 'What is the capital of France?',
+      questionText: "What is the capital of France?",
       answerOptions: [
-        { answerText: 'New York', isCorrect: false },
-        { answerText: 'London', isCorrect: false },
-        { answerText: 'Paris', isCorrect: true },
-        { answerText: 'Dublin', isCorrect: false },
+        { answerText: "New York", isCorrect: false },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "Dublin", isCorrect: false },
       ],
     },
     {
-      questionText: 'Who is CEO of Tesla?',
+      questionText: "Who is CEO of Tesla?",
       answerOptions: [
-        { answerText: 'Jeff Bezos', isCorrect: false },
-        { answerText: 'Elon Musk', isCorrect: true },
-        { answerText: 'Bill Gates', isCorrect: false },
-        { answerText: 'Tony Stark', isCorrect: false },
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
       ],
     },
     {
-      questionText: 'The iPhone was created by which company?',
+      questionText: "The iPhone was created by which company?",
       answerOptions: [
-        { answerText: 'Apple', isCorrect: true },
-        { answerText: 'Intel', isCorrect: false },
-        { answerText: 'Amazon', isCorrect: false },
-        { answerText: 'Microsoft', isCorrect: false },
+        { answerText: "Apple", isCorrect: true },
+        { answerText: "Intel", isCorrect: false },
+        { answerText: "Amazon", isCorrect: false },
+        { answerText: "Microsoft", isCorrect: false },
       ],
     },
     {
-      questionText: 'How many Harry Potter books are there?',
+      questionText: "How many Harry Potter books are there?",
       answerOptions: [
-        { answerText: '1', isCorrect: false },
-        { answerText: '4', isCorrect: false },
-        { answerText: '6', isCorrect: false },
-        { answerText: '7', isCorrect: true },
+        { answerText: "1", isCorrect: false },
+        { answerText: "4", isCorrect: false },
+        { answerText: "6", isCorrect: false },
+        { answerText: "7", isCorrect: true },
       ],
     },
   ];
@@ -59,22 +60,24 @@ const Quiz = () => {
   };
   return (
     <Container mt={24} centerContent>
-      <div className='app'>
+      <div className="app">
         {showScore ? (
-          <div className='score-section'>
+          <div className="score-section">
             You scored {score} out of {questions.length}
           </div>
         ) : (
           <>
-            <div className='question-section'>
-              <div className='question-count'>
+            <div className="question-section">
+              <div className="question-count">
                 <span>Question {currentQuestion + 1}</span>/{questions.length}
               </div>
-              <div className='question-text'>{questions[currentQuestion].questionText}</div>
+              <div className="question-text">{questions[currentQuestion].questionText}</div>
             </div>
-            <div className='answer-section'>
+            <div className="answer-section">
               {questions[currentQuestion].answerOptions.map((answerOption) => (
-                <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+                <button className="btn" onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>
+                  {answerOption.answerText}
+                </button>
               ))}
             </div>
           </>
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -8,47 +8,47 @@ import Quiz from "../pages/quiz";
 import Watch from "../pages/watch";
 import Adventure from "../pages/read/adventure";
 import Nature from "../pages/read/nature";
-import Vocab from '../component/mainVocab';
+import Vocab from "../component/mainVocab";
 import VocabContent from "../component/vocabItems/content";
 import BookContent from "../component/books/content";
 import EmbedYoutubeVideoContent from "../component/watch/embedYTVideo/content";
 
 const AppRouter = () => {
-    return (
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/">
-                    <Route index element={<Home />} />
-                    <Route path="read">
-                        <Route index element={<Read />} />
-                        <Route path="adventure">
-                            <Route index element={<Adventure />} />
-                            <Route path=":id" element={<BookContent />} />
-                        </Route>
-                        <Route path="nature">
-                            <Route index element={<Nature />} />
-                            <Route path=":id" element={<BookContent />} />
-                        </Route>
-                    </Route>
-                    <Route path="learn">
-                        <Route index element={<Learn />} />
-                        <Route path="vocabs">
-                            <Route index element={<Vocab />} />
-                            <Route path=":id" element={<VocabContent />} />
-                        </Route>
-                    </Route>
-                    <Route path="watch">
-                        <Route index element={<Watch />} />
-                        <Route path=":id" element={<EmbedYoutubeVideoContent/>} />
-                    </Route>
-                    <Route path="quiz" element={<Quiz />} />
-                    <Route path="*" element={<div>nothing here code:404</div>}></Route>
-                </Route>
-            </Routes>
-            <Footer />
-        </Router>
-    )
-}
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="read">
+            <Route index element={<Read />} />
+            <Route path="adventure">
+              <Route index element={<Adventure />} />
+              <Route path=":id" element={<BookContent />} />
+            </Route>
+            <Route path="nature">
+              <Route index element={<Nature />} />
+              <Route path=":id" element={<BookContent />} />
+            </Route>
+          </Route>
+          <Route path="learn">
+            <Route index element={<Learn />} />
+            <Route path="vocabs">
+              <Route index element={<Vocab />} />
+              <Route path=":id" element={<VocabContent />} />
+            </Route>
+          </Route>
+          <Route path="watch">
+            <Route index element={<Watch />} />
+            <Route path=":id" element={<EmbedYoutubeVideoContent />} />
+          </Route>
+          <Route path="quiz" element={<Quiz />} />
+          <Route path="*" element={<div>nothing here code:404</div>}></Route>
+        </Route>
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
 
 export default AppRouter;
